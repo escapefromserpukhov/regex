2f97b29db55dae1dfb222f42995a07f10767891f
Viktoria_finch <85229737+escapefromserpukhov@users.noreply.github.com>
2023-06-15 10:52:29 +0300

    Update nicknames.js

--- src/js/__tests__/nicknames.js
+++ src/js/__tests__/nicknames.js
@@ -1,3 +1,61 @@
-test('check 1', () => {
-  // expect(result(args)).toEqual(expected);
+import Validator from '../basic';
+
+test('Объект должен пропускать логин', () => {
+  const login = new Validator('Masha');
+
+  expect(login.validateUsername()).toBe(true);
+});
+
+test('Объект должен пропускать логин', () => {
+  const login = new Validator('Ma-s-ha');
+
+  expect(login.validateUsername()).toBe(true);
+});
+
+test('Объект должен пропускать логин', () => {
+  const login = new Validator('Ma_s-hA');
+
+  expect(login.validateUsername()).toBe(true);
+});
+
+test('Объект должен пропускать логин', () => {
+  const login = new Validator('Ma55hA');
+
+  expect(login.validateUsername()).toBe(true);
+});
+
+test('Объект должен пропускать логин', () => {
+  const login = new Validator('Ma555-hA');
+
+  expect(login.validateUsername()).toBe(true);
+});
+
+test('Объект должен пропускать логин', () => {
+  const login = new Validator('Ma555-555hA');
+
+  expect(login.validateUsername()).toBe(true);
+});
+
+test('Объект не должен пропускать логин', () => {
+  const login = new Validator('Masha11');
+
+  expect(login.validateUsername()).toBe(false);
+});
+
+test('Объект не должен пропускать логин', () => {
+  const login = new Validator('_Masha11');
+
+  expect(login.validateUsername()).toBe(false);
+});
+
+test('Объект не должен пропускать логин', () => {
+  const login = new Validator('Masha_');
+
+  expect(login.validateUsername()).toBe(false);
+});
+
+test('Объект не должен пропускать логин', () => {
+  const login = new Validator('Ma_s-hA$');
+
+  expect(login.validateUsername()).toBe(false);
 });
