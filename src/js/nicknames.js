d96d964e190cc753501b0d9b1c5e469102fed5ea
Viktoria_finch <85229737+escapefromserpukhov@users.noreply.github.com>
2023-06-15 10:52:08 +0300

    Delete nicknames.js

--- src/js/nicknames.js
+++
@@ -1,9 +0,0 @@
-export default class Validator {
-  constructor(username) {
-    this.username = String(username);
-  }
-
-  validateUsername() {
-    return /^[a-z]+[a-z-_\d{0,3}]*[a-z]+$/i.test(this.username);
-  }
-}
